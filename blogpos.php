fa7ba12ef218a93265f7bea46bffd7b31f3c0f7a
[lhain] <[lutz.hain@web.de]>
2019-10-25 14:21:41 +0200

    test

--- blogpos.php
+++ blogpos.php
@@ -1,8 +1,9 @@
-        <div id="blogpos">  
-            <?php   // bindet in Joomla eine Modulposition unterhalb des Leading-Artikels ein
-                $modules =& JModuleHelper::getModules('blogpos');
-                foreach ($modules as $module){
-                echo JModuleHelper::renderModule($module);
-                }
-            ?>
-        </div>
+<div id="blogpos">  
+    <?php   // bindet in Joomla eine Modulposition unterhalb des Leading-Artikels ein
+        $modules =& JModuleHelper::getModules('blogpos');
+        foreach ($modules as $module){
+        echo JModuleHelper::renderModule($module);
+        }
+    ?>
+    <!--Änderung-->
+</div>
